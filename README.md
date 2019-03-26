a4407e7f435c694a5fc879828fa6cc0e868ed9f7
Bradley Hanson <brad@bradhanson.com>
2019-03-26 16:30:38 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# H-Tree Construction
+
+https://en.wikipedia.org/wiki/H_tree
+
+Originally implemented for a [Pramp interview](https://www.pramp.com/invt/lY9Lxp2vVZidqxz6qAGo). The Pramp interview platform did not include a proper `drawLine()` function, so I implemented this along with tests to verify the correctness of my solution.
+
+My original solution as coded in the allocated 30~ minute interview turned out to be mostly correct, but had a couple <strike>errors</strike> opportunities for improvement:
+
+Opportunities for improvement:
+
+- I misunderstood the scaling coefficient. I took root of line length instead of diving line length by √2.
+- I did not account for depth = 0.
+
+I noticed the scaling coefficient mistake after I implemented the drawing function and visually it did not look correct. However, even after fixing the mistake it still did not look correct. This lead me to discovering an error in the Pramp H-Tree-Construction specification:
+
+> It can be constructed by starting with a line segment of arbitrary length, **drawing two segments of the same length** at right angles to the first through its endpoints, and continuing in the same vein, reducing (dividing) the length of the line segments drawn at each stage by √2.
+
+Here is the correct specification from Wikipedia:
+
+> An H tree can be constructed by starting with a line segment of arbitrary length, **drawing two shorter segments** at right angles to the first through its endpoints, and continuing in the same vein, reducing (dividing) the length of the line segments drawn at each stage by √2.
+
+I acknowledge that they could have simplified the problem for interview purposes.
---
+++ main.js
@@ -0,0 +1,85 @@
+'use strict';
+
+function drawHTree(context, x, y, length, depth) {
+  if (Number(depth) === 0) {
+    return;
+  }
+
+  const lineSegment = length / 2;
+
+  // draw horizontal line
+  let leftPointX = x - lineSegment;
+  let leftPointY = y;
+  let rightPointX = x + lineSegment;
+  let rightPointY = y;
+  drawLine(context, leftPointX, leftPointY, rightPointX, rightPointY);
+
+  const nextLength = length / Math.sqrt(2);
+  const nextSegment = nextLength / 2;
+
+  // draw left vertical line
+  let upPointX = x - lineSegment; // also starting points for next calls
+  let upPointY = y + nextSegment;
+  let downPointX = x - lineSegment;
+  let downPointY = y - nextSegment;
+  drawLine(context, upPointX, upPointY, downPointX, downPointY);
+
+  // draw right vertical line
+  let upPoint2X = x + lineSegment; // also starting points for next calls
+  let upPoint2Y = y + nextSegment;
+  let downPoint2X = x + lineSegment;
+  let downPoint2Y = y - nextSegment;
+  drawLine(context, upPoint2X, upPoint2Y, downPoint2X, downPoint2Y);
+
+  if (depth > 1) {
+    const newLength = nextLength / Math.sqrt(2);
+    drawHTree(context, upPointX, upPointY, newLength, depth - 1);
+    drawHTree(context, downPointX, downPointY, newLength, depth - 1);
+    drawHTree(context, upPoint2X, upPoint2Y, newLength, depth - 1);
+    drawHTree(context, downPoint2X, downPoint2Y, newLength, depth - 1);
+  }
+}
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.moveTo(context.canvas.width / 2 + x1, context.canvas.height / 2 + y1);
+  context.lineTo(context.canvas.width / 2 + x2, context.canvas.height / 2 + y2);
+  context.stroke();
+}
+
+const canvas = document.getElementById('game');
+if (canvas.getContext) {
+  const context = canvas.getContext('2d');
+
+  context.canvas.width = 800;
+  context.canvas.height = 600;
+
+  // draw border
+  context.strokeRect(0, 0, context.canvas.width, context.canvas.height);
+
+  const decrementButton = document.getElementById('decrementButton');
+  const incrementButton = document.getElementById('incrementButton');
+  const depthView = document.getElementById('depthView');
+
+  decrementButton.addEventListener('click', function() {
+    if (depthView.innerHTML === '0') {
+      return;
+    }
+    depthView.innerHTML = Number(depthView.innerHTML) - 1;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.strokeRect(0, 0, context.canvas.width, context.canvas.height);
+    drawHTree(context, 0, 0, 350, depthView.innerHTML);
+  });
+
+  incrementButton.addEventListener('click', function() {
+    if (depthView.innerHTML === '7') {
+      return;
+    }
+    depthView.innerHTML = Number(depthView.innerHTML) + 1;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.strokeRect(0, 0, context.canvas.width, context.canvas.height);
+    drawHTree(context, 0, 0, 350, depthView.innerHTML);
+  });
+
+  drawHTree(context, 0, 0, 350, depthView.innerHTML);
+}
